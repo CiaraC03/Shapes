65016f3e0de98539911b4bada896258141182d9f
Ciara Crowe <G00402357@atu.ie>
2022-10-18 13:49:43 +0100

    Added new file called Rectangle

--- src/main/java/ie/atu/week5/Rectangle.java
+++ src/main/java/ie/atu/week5/Rectangle.java
@@ -1,4 +1,18 @@
 package ie.atu.week5;
 
 public class Rectangle {
+    private double length;
+    private double width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length){
+        this.length = length;
+
+    }
+    public void setWidth(double width){
+        this.width = width;
+    }
 }
