6a5aa7fedc2d99756ea6de45ab4cbf7dccdd87a9
Ciara Crowe <G00402357@atu.ie>
2022-10-18 14:36:49 +0100

    Added new methods

--- src/main/java/ie/atu/week5/ShareApp.java
+++ src/main/java/ie/atu/week5/ShareApp.java
@@ -5,10 +5,35 @@ import java.util.Scanner;
 
 public class ShareApp {
     public static void main(String[] args) {
-        System.out.println("Please enter the width of the rectangle");
+        //Created first instance of rectangle
+        System.out.println("Please enter the length of the rectangle");
         Scanner input = new Scanner(System.in);
         double length = input.nextDouble();
-        System.out.println("You entered : " + length);
+        Rectangle shapeRect = new Rectangle();
+        shapeRect.setLength(length);
+        System.out.println("You entered : " + shapeRect.getLength());
 
+
+
+        System.out.println("Please enter the width of the rectangle");
+        double width = input.nextDouble();
+        shapeRect.setWidth(width);
+        System.out.println("You entered : " + shapeRect.getWidth());
+
+        //Created second instance of rectangle
+        Rectangle shapeRect2 = new Rectangle();
+        System.out.println("Please enter second the length of the rectangle");
+        double length2 = input.nextDouble();
+        shapeRect2.setLength(length2);
+        System.out.println("You entered : " + shapeRect2.getLength());
+
+
+
+        System.out.println("Please enter the width of the rectangle");
+        double width2 = input.nextDouble();
+        shapeRect2.setWidth(width2);
+        System.out.println("You entered : " + shapeRect2.getWidth());
     }
+
+
 }
